74e0a073319716f7f9e1cef7a485c6c6d85791e2
lucasprezom <lucasprezom@gmail.com>
2020-08-18 19:35:48 -0300

    Segundo commit

--- mercado.js
+++ mercado.js
@@ -1,4 +1,6 @@
-const rs = require("readline-sync")
+// const rs = require("readline-sync")
+
+// const axios = require('axios')
 
 function carregarProdutos() {
     let divProdutos = document.getElementById("todosOsProdutos")
@@ -9,32 +11,16 @@ function carregarProdutos() {
     
     for (let i = 0; i <= produtos.length; i++) {
         let produtoAtual = document.createElement("div")
-        produtoAtual.innerHTML = `
-        <div class="card col-lg-3">
-  
-          <article>
-            <div class="imagens">
-              <img src="${produtos.foto}" alt="${produtos.nome}">
-            </div>
-            <i class="heart">
-              <img width="20" src="http://www.onlygfx.com/wp-content/uploads/2017/08/red-watercolor-heart-2-1-300x267.png"
-                alt="">
-            </i>
-  
-            <button class="trucky">
-              <img width="30" src="https://cdn3.iconfinder.com/data/icons/sympletts-part-10/128/truck-speed-512.png"
-                alt="lil truck">
-            </button>
-          </article>
-          <article class="price">
-            <h2>${produtos.preco}</h2>
-            <input class="quantidade">
-        <button id="comprarAlface" type="button" class="btn btn-outline-success">Comprar</button>
-          </article>
-          <article class="description">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </article>
-        </div>`
+      produtoAtual.innerHTML = `
+        <div class="row">
+      <div class="column">
+        <div class="card">
+          <img src="${produtos.foto}" alt="${produtos.nome}">
+          <input class="quantidade">
+          <h2>${produtos.preco}</h2>
+          <button id="comprar${produtos.nome}" type="button" class="btn btn-outline-success">Comprar</button>
+        </div>
+      </div>`
         divProdutos.appendChild(produtoAtual)
     }
 }
